431b6791a57d2efd10028ff3a2aea7fff53e9cfc
cagryinside <cagricetin89@gmail.com>
2019-05-27 15:17:37 -0400

    Add text data type to Open311 data types

--- src/main/java/edu/usf/cutr/open311client/constants/Open311DataType.java
+++ src/main/java/edu/usf/cutr/open311client/constants/Open311DataType.java
@@ -24,6 +24,7 @@ package edu.usf.cutr.open311client.constants;
  */
 public class Open311DataType {
   public static final String STRING = "string";
+  public static final String TEXT = "text";
   public static final String NUMBER = "number";
   public static final String DATETIME = "datetime";
   public static final String SINGLEVALUELIST = "singlevaluelist";
